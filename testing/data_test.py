b4339fdc8fd52d7fef724870d949a5c9ea2d17f1
Glodgar <glodgarek@gmail.com>
2022-07-24 00:13:56 +0200

    update data_test.py

--- testing/data_test.py
+++ testing/data_test.py
@@ -1,7 +1,11 @@
 t_criteriaCount = 5
+
 t_criteriaList = ["k1", "k2", "k3", "k4", "k5"]
+
 t_alternativesCount = 3
+
 t_alternativesList  = ["mBank", "Millenium", "BPH"]
+
 t_criteriaMatrix = [
     [1.0, 0.143, 0.333, 3.0, 0.2],
     [7.0, 1.0, 5.0, 9.0, 3.0],
@@ -9,7 +13,7 @@ t_criteriaMatrix = [
     [0.333, 0.111, 0.2, 1.0, 0.143],
     [5.0, 0.333, 3.0, 7.0, 1.0]
 ]
-#criteriaFactors =
+
 t_alternativesMatrix = [
     [[1.0, 0.333, 0.2],
     [3.0, 1.0, 0.333],
@@ -31,5 +35,3 @@ t_alternativesMatrix = [
     [0.333, 1.0, 0.143],
     [5.0, 7.0, 1.0]]
 ]
-#alternativesFactors =
-#prediction =
